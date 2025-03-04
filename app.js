5b78352c14679c5f29604e8f82ba6c238927d740
yuanjing <jing.yuan@kapeixi.com>
2025-03-04 14:02:31 +0800

    .

--- app.js
+++ app.js
@@ -2,21 +2,21 @@ const express = require('express');
 const path = require('path');
 const app = express();
 // 在 app.js 中引入路由模块
-const routes = require('./routes/index.js');
+const routes = require('./routes');
 const config = require('config');
 const port = config.get('port');
 const logger = require('./middlewares/logger');
 
-// 使用日志中间件
-app.use(logger);
-
-app.use('/', routes);
-// 设置视图引擎为 EJS
-app.set('view engine', 'ejs');
 // 设置静态文件夹
 app.use(express.static(path.join(__dirname, 'public')));
+
+// 设置视图引擎为 EJS
+app.set('view engine', 'ejs');
 // 设置视图文件夹
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'views/pages'));
+app.use('/', routes);
+// 使用日志中间件
+app.use(logger);
 
 // 使用配置
 app.listen(port, () => {
--- routes/home.js
+++ routes/home.js
@@ -6,6 +6,7 @@ const router = express.Router();
 
 // 定义用户列表路由
 router.get('/', async (req, res, next) => {
+  console.log("123123123")
   console.log('用户列表');
   res.send('首页');
   next();
