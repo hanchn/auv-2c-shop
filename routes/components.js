04535b199b8c8e46f654d50ecdb634d0bce1087b
yuanjing <jing.yuan@kapeixi.com>
2025-03-04 14:17:02 +0800

    .

--- routes/components.js
+++ routes/components.js
@@ -7,14 +7,14 @@ const router = express.Router();
 // 定义用户列表路由
 router.get('/', async (req, res, next) => {
   console.log('' + req.params.id);
-  res.render('components', { title: '关于我们', message: '了解更多关于我们的信息。' });
+  res.render('test', { title: '关于我们', message: '了解更多关于我们的信息。' });
   next();
 });
 
 // 定义用户列表路由
 router.get('/:id', async (req, res, next) => {
   console.log('' + req.params.id);
-  res.render('components', { title: '关于我们', message: '了解更多关于我们的信息。' });
+  res.render('test', { title: '关于我们', message: '了解更多关于我们的信息。' });
   next();
 });
 
