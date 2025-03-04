5eea9896471d879e217b80f1c245ece3119e00dd
yuanjing <jing.yuan@kapeixi.com>
2025-03-04 11:31:36 +0800

    .

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const path = require('path');
 const app = express();
 // 在 app.js 中引入路由模块
 const indexRouter = require('./routes/index.js');
+const config = require('config');
+const port = config.get('port');
 
 app.use('/', indexRouter);
 // 设置视图引擎为 EJS
@@ -11,3 +13,8 @@ app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 // 设置视图文件夹
 app.set('views', path.join(__dirname, 'views'));
+
+// 使用配置
+app.listen(port, () => {
+  console.log(`服务器运行在端口：${port}`);
+});
